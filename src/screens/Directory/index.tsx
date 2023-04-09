b79b19a8ac5dbdb9024776cb976ea4bd89295e81
udith-shalinda <udithshalinda2@gmail.com>
2023-04-09 19:22:41 +0530

    logout button added

--- src/screens/Directory/index.tsx
+++ src/screens/Directory/index.tsx
@@ -1,10 +1,26 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {StackActions, useNavigation} from '@react-navigation/native';
 import * as React from 'react';
-import {SafeAreaView, ScrollView} from 'react-native';
+import {SafeAreaView, ScrollView, View} from 'react-native';
+import {X_Button} from '../../components/Button';
 import {MyHeader} from '../../components/MyHeader';
 import {ParticipantElement} from '../../components/ParticipantElement';
-import {routes} from '../../constants';
+import {ACCESS_TOKEN, colors, routes} from '../../constants';
+import {useAppDispatch} from '../../hooks/useRedux';
+import {userSlice} from '../../store/user';
+import {styles} from './style';
 
 export const Directory = () => {
+  const dispatch = useAppDispatch();
+  const navigation = useNavigation();
+
+  const logout = async () => {
+    AsyncStorage.setItem(ACCESS_TOKEN, '');
+    dispatch(userSlice.actions.setUser(null));
+    navigation.dispatch(StackActions?.replace(routes.login));
+    // remove app id from db
+  };
+
   return (
     <SafeAreaView>
       <MyHeader title="Directory" />
@@ -40,6 +56,15 @@ export const Directory = () => {
           title="Board Members"
           imageUrl="https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
         />
+        <View style={styles.buttonWrapper}>
+          <X_Button
+            title="Log Out"
+            color={colors.white}
+            onPress={logout}
+            backgroundColor={colors.error}
+            borderWidth={0}
+          />
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
---
+++ src/screens/Directory/style.ts
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+import {colors} from '../../constants';
+
+export const styles = StyleSheet.create({
+  buttonWrapper: {
+    marginVertical: 15,
+    marginHorizontal: 15,
+  },
+});
