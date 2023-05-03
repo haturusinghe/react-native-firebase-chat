9dce3167e892b7b4d1ea6f510931b1a2dd16534f
S.Haturusinghe <haturusi-cs18015@stu.kln.ac.lk>
2023-05-03 13:48:55 +0530

    updated Chat Screen to display current users opened conversations (gets the from firestore)

--- src/screens/Chat/index.tsx
+++ src/screens/Chat/index.tsx
@@ -1,71 +1,112 @@
 import {useRoute} from '@react-navigation/native';
 import * as React from 'react';
 import {SafeAreaView, ScrollView} from 'react-native';
-import {ChatElement} from '../../components/ChatElement';
+import {ChatElement as ChatCard} from '../../components/ChatElement';
 import {MyHeader} from '../../components/MyHeader';
+import firestore from '@react-native-firebase/firestore';
+import {useState, useEffect} from 'react';
+import {useAppSelector} from '../../hooks/useRedux';
+
+interface ChatData {
+  id: string;
+  name?: string;
+  avatar?: string;
+  lastMessage?: string;
+  lastMessageTime?: Date;
+  users?: UserData[];
+  userIds?: string[];
+}
+
+interface UserData {
+  _id: string;
+  name: string;
+  avatar: string;
+}
 
 export const Chat = () => {
-  const route = useRoute<any>();
+  const {user} = useAppSelector(store => store.user);
+
+  const [chatUser, setChatUser] = useState<UserData>({
+    _id: user?._id || 'no-id',
+    name: user?.name || '',
+    avatar:
+      user?.imageUrl ||
+      'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png',
+  });
+
+  const [chats, setChats] = useState<ChatData[]>([]);
+
+  useEffect(() => {
+    const query = firestore()
+      .collection('chats')
+      .where('userIds', 'array-contains', chatUser._id);
+
+    const unsubscribe = query.onSnapshot(querySnapshot => {
+      //
+      // get a snapshot of the chats collection
+      const chatsData = querySnapshot.docs.map(documentSnapshot => {
+        const data = documentSnapshot.data();
+
+        // get the last message time
+        const lastMessageTime = data.lastMessageTime
+          ? new Date(data.lastMessageTime.seconds * 1000)
+          : undefined;
+
+        // get the users
+        const users = data.users
+          ? data.users.map((user: any) => ({
+              _id: user._id,
+              name: user.name,
+              avatar: user.avatar,
+            }))
+          : undefined;
+
+        // get the user ids
+        const userIds: string[] = data.userIds || [];
+
+        //returns the chat data
+        return {
+          id: documentSnapshot.id,
+          lastMessage: data.lastMessage,
+          lastMessageTime,
+          users,
+          userIds,
+        };
+      });
+
+      console.log('chatsData', chatsData);
+      setChats(chatsData);
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
     <SafeAreaView>
       <MyHeader title={'Chat'} />
       <ScrollView>
-        <ChatElement
-          round={route?.params?.round}
-          url={route?.params?.url}
-          title="Mr. Rabi Sighj"
-          subTitle="Hello how are you"
-          text="04:34pm"
-          imageUrl="https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        />
-        <ChatElement
-          round={route?.params?.round}
-          url={route?.params?.url}
-          title="Mr. Rabi Sighj"
-          subTitle="Thank you!!"
-          text="12:30pm"
-          imageUrl="https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        />
-        <ChatElement
-          round={route?.params?.round}
-          url={route?.params?.url}
-          title="Mr. Rabi Sighj"
-          subTitle="Thank you!!"
-          text="12:30pm"
-          imageUrl="https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        />
-        <ChatElement
-          round={route?.params?.round}
-          url={route?.params?.url}
-          title="Mr. Rabi Sighj"
-          subTitle="Thank you!!"
-          text="12:30pm"
-          imageUrl="https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        />
-        <ChatElement
-          round={route?.params?.round}
-          url={route?.params?.url}
-          title="Mr. Rabi Sighj"
-          subTitle="Thank you!!"
-          text="12:30pm"
-          imageUrl="https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        />
-        <ChatElement
-          round={route?.params?.round}
-          url={route?.params?.url}
-          title="Mr. Rabi Sighj"
-          subTitle="Thank you!!"
-          text="12:30pm"
-          imageUrl="https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        />
-        <ChatElement
-          round={route?.params?.round}
-          url={route?.params?.url}
-          title="Mr. Rabi Sighj"
-          subTitle="Thank you!!"
-          text="12:30pm"
-          imageUrl="https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        />
+        {chats.length >= 1 ? (
+          chats.map(chat => (
+            <ChatCard
+              key={chat.id}
+              recipientName={
+                chat.users?.filter(user => user._id !== chatUser._id)[0]
+                  ?.name || 'No name'
+              }
+              lastMessage={chat.lastMessage}
+              lastMessageTime={chat.lastMessageTime?.toDateString() || ''}
+              _id={
+                chat.userIds?.filter(id => id !== chatUser._id)[0] || 'no-id'
+              }
+            />
+          ))
+        ) : (
+          <ChatCard
+            recipientName={'No name'}
+            lastMessage={'No message'}
+            lastMessageTime={''}
+            _id={'no-id'}
+          />
+        )}
       </ScrollView>
     </SafeAreaView>
   );
